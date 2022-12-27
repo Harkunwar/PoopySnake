f66188f6ccf69ea14eeb2ec665563e3f0d6a4f7d
Harkunwar Singh Kochar <geek@harkunwar.com>
2022-12-27 13:47:30 -0800

    try cloudflare setup sh

---
+++ cloudflare.setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source "$HOME/.cargo/env"
+PATH=\"$HOME/.cargo/bin:$PATH\" curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
