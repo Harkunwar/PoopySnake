e50a16d3f3108ab3f39ba8cdf56b03800279ff01
Harkunwar Singh Kochar <geek@harkunwar.com>
2022-12-27 14:24:02 -0800

    wasm-bindgen-cli not required

--- cloudflare.setup.sh
+++ cloudflare.setup.sh
@@ -2,6 +2,5 @@
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
-cargo install wasm-bindgen-cli
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 yarn build
