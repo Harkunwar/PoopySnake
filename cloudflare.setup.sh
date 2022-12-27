5c033d3fd3561c1a0fccbbacc3ac01db25d2f535
Harkunwar Singh Kochar <geek@harkunwar.com>
2022-12-27 14:20:14 -0800

    build in cloudflare.setup.sh

--- cloudflare.setup.sh
+++ cloudflare.setup.sh
@@ -3,3 +3,4 @@
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
 PATH=\"$HOME/.cargo/bin:$PATH\" curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+yarn build
